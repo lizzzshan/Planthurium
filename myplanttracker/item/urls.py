3b5a7e6f8d8313ce835107ee3468f562120eb79d
lizzzshan <odysuessrox@gmail.com>
2023-09-05 14:21:34 -0700

    Implement search feature and clear filter feature

--- myplanttracker/item/urls.py
+++ myplanttracker/item/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = 'item'
 
 urlpatterns = [
+    path('', views.items, name='items'),
     path('new/', views.new, name='new'),
     path('<int:pk>/', views.detail, name='detail'),
     path('<int:pk>/delete/', views.delete, name='delete'),
